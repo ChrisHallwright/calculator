50b36244211a90b700e5dd47a7ff6144c7a82803
Chris Hallwright <38396526+ChrisHallwright@users.noreply.github.com>
2026-09-18 08:07:14 +1000

    Add initial key functionality for digits, operators and equal sign

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function divide(a, b) {
 
 let numA;
 let numB;
-let operator;
+let op;
 
 function operate(a, op, b) {
     switch (op) {
@@ -36,3 +36,53 @@ function operate(a, op, b) {
             break;
     }
 }
+
+function getDigit(d) {
+    let io = document.querySelector('.io');
+    io.textContent = `${io.textContent}${d}`;
+}
+
+function getOp(o) {
+    let io = document.querySelector('.io');
+    numA = parseInt(io.textContent);
+    io.textContent = `${io.textContent}${o}`;
+    switch (o) {
+        case '/':
+            op = 'divide';
+            break;
+        case 'x':
+            op = 'multiply';
+            break;
+        case '-':
+            op = 'subtract';
+            break;
+        case '+':
+            op = 'add';
+            break;
+    }
+}
+
+function calc() {
+    let io = document.querySelector('.io');
+    numB = parseInt(io.textContent.match(/[x\+-\/].+/)[0].slice(1));
+    let result = operate(numA, op, numB);
+    let inputs = document.querySelector('.inputs');
+    inputs.textContent = io.textContent;
+    io.textContent = result;
+}
+
+let digits = document.querySelectorAll('.num');
+digits.forEach(digitBtn => {
+    if (digitBtn.textContent != '+/-') {
+        digitBtn.addEventListener('click', () => getDigit(digitBtn.textContent));
+    }
+}); 
+
+let operators = document.querySelectorAll('.op');
+operators.forEach(opBtn => {
+    if (opBtn.textContent === '=') {
+        opBtn.addEventListener('click', () => calc());
+    } else {
+        opBtn.addEventListener('click', () => getOp(opBtn.textContent));
+    }
+}); 
