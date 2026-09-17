6d5b1c953f793f0e26c1881b494e08cb638cda04
Chris Hallwright <38396526+ChrisHallwright@users.noreply.github.com>
2026-09-17 22:12:36 +1000

    Perform arithmetic functions and lay out calculator

---
+++ script.js
@@ -0,0 +1,38 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b != 0) {
+        return a / b;
+    }
+}
+
+let numA;
+let numB;
+let operator;
+
+function operate(a, op, b) {
+    switch (op) {
+        case 'add':
+            return add(a, b);
+            break;
+        case 'subtract':
+            return subtract(a, b);
+            break;
+        case 'multiply':
+            return multiply(a, b);
+            break;
+        case 'divide':
+            return divide(a, b);
+            break;
+    }
+}
